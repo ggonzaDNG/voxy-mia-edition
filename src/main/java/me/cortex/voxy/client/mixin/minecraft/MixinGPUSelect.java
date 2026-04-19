205b41a0f9683f40e795454c0f337e4dba1048f3
mcrcortex <18544518+mcrcortex@users.noreply.github.com>
2026-04-19 17:43:44 +1000

    move and hoist gpu selection injection

--- src/main/java/me/cortex/voxy/client/mixin/minecraft/MixinGPUSelect.java
+++ src/main/java/me/cortex/voxy/client/mixin/minecraft/MixinGPUSelect.java
@@ -6,15 +6,18 @@ import com.mojang.blaze3d.platform.Window;
 import com.mojang.blaze3d.platform.WindowEventHandler;
 import me.cortex.voxy.client.GPUSelectorWindows2;
 import me.cortex.voxy.common.util.ThreadUtils;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.main.GameConfig;
+import net.minecraft.world.entity.vehicle.minecart.Minecart;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Window.class)
-public class MixinWindow {
-    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/Window;setBootErrorCallback()V"))
-    private void voxy$injectInitWindow(WindowEventHandler eventHandler, ScreenManager monitorTracker, DisplayData settings, String fullscreenVideoMode, String title, CallbackInfo ci) {
+@Mixin(Minecraft.class)
+public class MixinGPUSelect {
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Options;save()V"))
+    private void voxy$injectInitWindow(GameConfig gc, CallbackInfo ci) {
         //System.load("C:\\Program Files\\RenderDoc\\renderdoc.dll");
         var prop = System.getProperty("voxy.forceGpuSelectionIndex", "NO");
         if (!prop.equals("NO")) {
